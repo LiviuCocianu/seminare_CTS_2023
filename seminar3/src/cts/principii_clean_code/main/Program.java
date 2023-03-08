c946fab0bb899944c71a1daa8d1dc9207c97318e
liviu <liviucocianu@gmail.com>
2023-03-08 09:10:23 +0200

    Test push

---
+++ seminar3/src/cts/principii_clean_code/main/Program.java
@@ -0,0 +1,20 @@
+package cts.principii_clean_code.main;
+
+import java.util.List;
+
+import cts.principii_clean_code.clase.Aplicant;
+import cts.principii_clean_code.clase.readers.AngajatReader;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Aplicant> listaAngajati;
+
+		AngajatReader ar = new AngajatReader();
+		listaAngajati = ar.readAplicants("angajati.txt");
+
+		for (Aplicant aplicant : listaAngajati)
+			System.out.println(aplicant.toString());
+	}
+
+}
